8f2f5cff7e0a034050d81dcc54f07e09cb2c5d7b
takao yajima <t-yajima@mapion.co.jp>
2014-02-10 13:12:37 +0900

    config.rb設定

--- Ayaka/config.rb
+++ Ayaka/config.rb
@@ -6,16 +6,14 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
-output_style = :expanded
-line_comments = false
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = "expanded"
+output_style = :compact
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = "true"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = "false"
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
